ed78bb60a16c73f17113d8645ae75cd0800dcdee
9schurke9 <schurke9@hotmail.de>
2017-02-17 12:22:40 +0100

    Server and Clientworking

--- 4.Semester/src/ClientA.java
+++ 4.Semester/src/ClientA.java
@@ -1,16 +1,23 @@
 import java.net.*;
 import java.io.*;
-
-public class ClientA {
-
+import java.util.Scanner;
+public class ClientA extends Thread {
+    public static Scanner eingabe = new Scanner(System.in);
     public static void main(String[] a){
+    new ClientA().start();
+
+    }
+    public void run(){
         try{
             Socket Anschluss = new Socket("localhost",8080);
             PrintWriter dos = new PrintWriter (Anschluss.getOutputStream(),true);
-            dos.println("Guten Tag und viel Spass!");
-            dos.flush();
+            while(true) {
+                dos.println("Client A:> "+eingabe.nextLine());
+                dos.flush();
+
+            }
         }catch(Exception e){
-        e.printStackTrace();
+            e.printStackTrace();
         }
     }
 }
--- 4.Semester/src/Server.java
+++ 4.Semester/src/Server.java
@@ -1,23 +1,38 @@
 import java.net.*;
 import java.lang.*;
 import java.io.*;
-public class Server {
-    private static boolean t = true;
-    private static String msg = null;
-    public static void main(String[] args) throws IOException{
-        ServerSocket anschluss = new ServerSocket(8080);
-        while (t) {
+import java.util.*;
+
+public class Server extends Thread {
+    private ArrayList<Object> clientlist = new ArrayList<Object>();
+    private String msg = null;
+
+    public static void main(String[] args) throws IOException {
+            new Server().start();
+    }
+
+    public void run() {
+        ServerSocket anschluss = null;
+
             try {
+                anschluss = new ServerSocket(8080);
+
                 Socket eavesdropping = anschluss.accept();
+
+                clientlist.add(eavesdropping);
+
                 InputStreamReader portreader = new InputStreamReader(eavesdropping.getInputStream());
                 BufferedReader input = new BufferedReader(portreader);
-                msg = input.readLine();
-                System.out.println(":> " + msg);
 
 
-            }catch(IOException i){
+                while(true) {
+
+                    msg = input.readLine();
+                    System.out.println("" + msg);
+                    System.out.println("");
+                }
+            } catch (IOException i) {
                 i.printStackTrace();
-            }
-    }
+        }
     }
 }
