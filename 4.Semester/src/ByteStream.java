21ff46c2e779481d1672fcb755f8722d2061db65
9schurke9 <schurke9@hotmail.de>
2017-03-09 14:42:34 +0100

    StreamUebung

--- 4.Semester/src/ByteStream.java
+++ 4.Semester/src/ByteStream.java
@@ -6,7 +6,23 @@ public class ByteStream {
     private byte[] buf = new byte[848029];
 
     public static void main(String[] a) throws IOException{
-        new ByteStream().readFromFile();
+       // new ByteStream().readFromFile();
+        new ByteStream().copyFile("G:/users/Progamer/Desktop/Bilder/..jpg","G:/users/Progamer/Desktop/Bilder/aaa.jpg");
+    }
+    public void copyFile(String from, String to)throws IOException{
+        BufferedInputStream bis = new BufferedInputStream((new FileInputStream(from)));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(to));
+
+        //how much is the byte of the File ?
+        int numbytes = bis.available();
+
+        //create array till that byte
+        byte[] buffer = new byte[numbytes];
+        //store every byte in the buffer(byte array), start at given offset, maximum number of bytes to read.
+        bis.read(buffer,0,numbytes);
+
+        bos.write(buffer);
+
     }
     public void readFromFile() throws IOException{
         BufferedInputStream bis = new BufferedInputStream(new FileInputStream("G:/Users/Progamer/Desktop/Bilder/..jpg"));
