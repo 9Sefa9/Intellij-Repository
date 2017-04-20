00657736bed566d8fbc2465e7bdf2538abee5398
Sefa Notebook <sefagoevercin@hotmail.de>
2017-04-20 20:53:19 +0200

    Uebung0: All lights on.  Sollte eigentlich funtkionieren, roman hat dabei geholfen.

--- 4.Semester/src/JavaFX/AllLightsOn.java
+++ 4.Semester/src/JavaFX/AllLightsOn.java
@@ -10,33 +10,61 @@ import javafx.stage.Stage;
 
 public class AllLightsOn extends Application {
     Button buttons[];
+    BorderPane root;
+    GridPane grid;
+    Scene scene;
 
     public static void main(String[] ar) {
-       launch(ar);
+        launch(ar);
     }
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        BorderPane root = new BorderPane();
-        GridPane grid = new GridPane();
-        Scene s = new Scene(root, 500, 600);
+        root = new BorderPane();
+        grid = new GridPane();
+        scene = new Scene(root, 500, 600);
 
         buttons = new Button[25];
+
         for (int i = 0; i < 25; i++) {
+            final int j = i;
             buttons[i] = new Button();
             buttons[i].setStyle("-fx-base:blue");
             buttons[i].setMinSize(100, 100);
 
             //spalte        //zeile
-            grid.add(buttons[i], i % 5, i / 5);
 
+            grid.add(buttons[i], i % 5, i / 5);
+            buttons[i].setOnAction(e -> {onClick(j);});
         }
+
         root.setTop(grid);
-        primaryStage.setScene(s);
+        primaryStage.setScene(scene);
         primaryStage.show();
 
     }
 
+    public void onClick(int i) {
 
+        changeColor(buttons[i]);
+
+        if (i % 5 != 4)
+            changeColor(buttons[i + 1]);
+        if (i % 5 != 0)
+            changeColor(buttons[i - 1]);
+        if (i / 5 != 4)
+            changeColor((buttons[i + 5]));
+        if (i / 5 != 0)
+            changeColor(buttons[i - 5]);
+
+        //   buttons[i].setOnAction(e -> {buttons[i].setStyle("-fx-base:yellow");
+
+
+    }
+
+    public void changeColor(Button button) {
+        String style = button.getStyle().equals("-fx-base:blue") ? "-fx-base:yellow" : "-fx-base:blue";
+        button.setStyle(style);
+    }
 }
