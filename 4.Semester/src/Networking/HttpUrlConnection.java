c77f40557ca0b8e121629d175b9a2fdc3c7c0b15
Sefa Laptop <sefagoevercin@hotmail.de>
2017-07-23 12:55:49 +0200

    ich bin sehr nah dran,..

--- 4.Semester/src/Networking/HttpUrlConnection.java
+++ 4.Semester/src/Networking/HttpUrlConnection.java
@@ -1,28 +1,53 @@
 package Networking;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.*;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.List;
+import java.util.logging.Level;
 
 public class HttpUrlConnection {
 
-    public static void submittingForm() throws Exception {
-       WebClient webClient = new WebClient();
+    public void submittingForm() throws Exception {
+
+        ignoreLogs();
+
+        final WebClient webClient = new WebClient(BrowserVersion.CHROME);
 
         HtmlPage page1 = webClient.getPage("https://moodle.uni-due.de/login/");
-       System.out.println(page1.asXml());
-        HtmlForm form = page1.getFormByName("body");
-        //HtmlInput input1 = page1.getBody().ge
-   //     input1.setValueAttribute("yarn");
+        System.out.println(page1.getTitleText());
+        HtmlElement body = page1.getBody();
+        final HtmlDivision div = (HtmlDivision)page1.getElementById("loginpanel");
 
-   //     HtmlSubmitInput submit1 = page1.getElementByName("btnG");
+        final List<?> form = page1.getForms();//getFormByName("login");
 
-   //     page1=submit1.click();
+        for(Object i: form)
+            System.out.println(i);
 
-//        System.out.println(page1.asXml());
+        final HtmlForm form2 = page1.getFirstByXPath("//form[@action='https://moodle.uni-due.de/login/index.php']");
+        final HtmlSubmitInput submitbutton = form2.getFirstByXPath("//input[@id='loginbtn']");
+        final HtmlTextInput usernameField =form2.getFirstByXPath("//input[@name='username']");
+        final HtmlTextInput passwordField =form2.getFirstByXPath("//input[@name='password']");
 
+        usernameField.setValueAttribute("sjsagoev");
+        passwordField.setValueAttribute("diabolo31213");
+
+        submitbutton.click();
         webClient.close();
     }
-    public static void main(String[] a) throws Exception{
-       submittingForm();
+
+    public void ignoreLogs() {
+        LogFactory.getFactory().setAttribute("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+
+        java.util.logging.Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+        java.util.logging.Logger.getLogger("org.apache.commons.httpclient").setLevel(Level.OFF);
+
+    }
+
+    public static void main(String[] a) throws Exception {
+        HttpUrlConnection url = new HttpUrlConnection();
+        url.submittingForm();
     }
 }
