0774497508dac2ee6aea88bd74455e5105b98a74
Sefa Computer <sefagoevercin@hotmail.de>
2017-08-08 20:48:40 +0200

    splitsort

--- 4.Semester/src/Algorithms/Main.java
+++ 4.Semester/src/Algorithms/Main.java
@@ -2,15 +2,17 @@ package Algorithms;
 
 public class Main {
     public  static  void  main(String[] args){
-        int[] test ={8, 3, 6, 4, 9, 1, 6, 3, 8, 5};
+        int[] test ={1, 5, 2, 3, 10, 6, 8, 9, 7,4};
         Splitsort s = new Splitsort(test);
         for(Integer i: test){
             System.out.print(i+", ");
         }
         System.out.println();
         s.split();
-        for(Integer i: test){
-            System.out.print(i+", ");
-        }
+
+            for (Integer i : test) {
+                System.out.print(i + ", ");
+            }
+
     }
 }
--- 4.Semester/src/Algorithms/Splitsort.java
+++ 4.Semester/src/Algorithms/Splitsort.java
@@ -14,60 +14,50 @@ public class Splitsort {
     /**
      * Splits the list in pieces
      */
-    public void split(){
-        int l = list.length-1;
-        int firsthalf = l/2;
-        int secondhalf = l/2;
+    public void split() {
+        int l = list.length - 1;
+        int firsthalf = l / list.length;
+        int secondhalf = l / list.length;
 
-        while(!((secondhalf-1)<0)){
+        while (((secondhalf - 1) > 0)) {
 
-            if(this.list[secondhalf] == this.list[secondhalf-1]){
-                secondhalf-=1;
+            if (this.list[secondhalf] == this.list[secondhalf - 1]) {
+                secondhalf -= 1;
+                continue;
             }
-            if (this.list[secondhalf]<this.list[secondhalf-1]){
-                secondhalf-=1;
-
+            if (this.list[secondhalf] < this.list[secondhalf - 1]) {
+                swap(list, secondhalf, secondhalf - 1);
+                secondhalf -= 1;
+                continue;
             }
-            if (this.list[secondhalf]<this.list[secondhalf-1]){
-                    swap(list,firsthalf+1,secondhalf);
-                    secondhalf-=1;
+            if (this.list[secondhalf] > this.list[secondhalf - 1]) {
+                secondhalf -= 1;
+                continue;
+            }
+
+        }
+
+            while ((firsthalf + 1) < list.length) {
+
+                if (this.list[firsthalf] == this.list[firsthalf + 1]) {
+                    firsthalf += 1;
                     continue;
                 }
-                secondhalf-=1;
-            }
-
-
-        while((firsthalf+1) < list.length){
-
-            if(this.list[firsthalf] == this.list[firsthalf+1]){
-                firsthalf+=1;
-
-            }
-            if (this.list[firsthalf]<this.list[firsthalf+1]){
-                firsthalf = firsthalf+1;
-
-            }
-            if (this.list[firsthalf]>this.list[firsthalf+1]){
-
-                    swap(list,firsthalf+1,secondhalf);
-                    firsthalf+=1;
+                if (this.list[firsthalf] < this.list[firsthalf + 1]) {
+                    firsthalf += 1;
                     continue;
                 }
-                firsthalf+=1;
+                if (this.list[firsthalf] > this.list[firsthalf + 1]) {
+                    swap(list, firsthalf, firsthalf+1);
+                    firsthalf += 1;
+                    continue;
+                }
+                firsthalf += 1;
             }
 
-            /*      8  3  6  4   9   1  6  3  8  5
-                                 ij
-
-                    8  3  6  4    1    9  6  3  8  5
-                             j         i
-
-                    8  3  6  4    1    9  6  3  8  5
-                          j               i
-
-                    8  3  3  4    1    8  6  6  8  5
-                    j                              i
-
+            /*
+                        1, 4, 2, 3,   7   , 6, 8, 9, 10,5
+                                      ij
 
             */
 
