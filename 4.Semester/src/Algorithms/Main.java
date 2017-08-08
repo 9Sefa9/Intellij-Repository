7c84073e1c72ea29ce97ddf6d0c90061c81967c5
Sefa Computer <sefagoevercin@hotmail.de>
2017-08-08 22:09:11 +0200

    trieng an alternative way

--- 4.Semester/src/Algorithms/Main.java
+++ 4.Semester/src/Algorithms/Main.java
@@ -1,18 +1,23 @@
 package Algorithms;
 
 public class Main {
-    public  static  void  main(String[] args){
-        int[] test ={1, 5, 2, 3, 10, 6, 8, 9, 7,4};
+    public  static  void  main(String[] args) throws InterruptedException {
+        int[] test ={23, 13, 2, 22, 10, 6, 8, 26, 7,4,30,1,21,19,27,17,5,12,18,15,16,20,3,25,24,9,29,14,28,11};
         Splitsort s = new Splitsort(test);
         for(Integer i: test){
             System.out.print(i+", ");
         }
         System.out.println();
-        s.split();
+        int b = 1;
+        while (true) {
 
-            for (Integer i : test) {
-                System.out.print(i + ", ");
-            }
+               s.alternateSplitSort();
+               for (Integer i : test) {
+                   System.out.print(i + ", ");
+               }
+               System.out.println("Anzahl Schritt: "+(b++));
 
+               Thread.sleep(3000);
+           }
     }
 }
--- 4.Semester/src/Algorithms/Splitsort.java
+++ 4.Semester/src/Algorithms/Splitsort.java
@@ -6,20 +6,23 @@ import java.util.ArrayList;
  * Splitsort Algorithm
  */
 public class Splitsort {
-
     private int[] list;
+    private int l,firsthalf,secondhalf;
+
     public Splitsort(int[] list){
+
         this.list = list;
+
+
     }
     /**
      * Splits the list in pieces
      */
     public void split() {
-        int l = list.length - 1;
-        int firsthalf = l / list.length;
-        int secondhalf = l / list.length;
+        firsthalf =  list.length/2;
+        secondhalf=  list.length/2;
 
-        while (((secondhalf - 1) > 0)) {
+        while (((secondhalf - 1) >= 0)) {
 
             if (this.list[secondhalf] == this.list[secondhalf - 1]) {
                 secondhalf -= 1;
@@ -34,9 +37,7 @@ public class Splitsort {
                 secondhalf -= 1;
                 continue;
             }
-
         }
-
             while ((firsthalf + 1) < list.length) {
 
                 if (this.list[firsthalf] == this.list[firsthalf + 1]) {
@@ -62,6 +63,44 @@ public class Splitsort {
             */
 
     }
+    public void alternateSplitSort(){
+        firsthalf  =  list.length/2;
+        secondhalf =  list.length/2;
+
+        while ((secondhalf % list.length)!=0) {
+
+            if (this.list[secondhalf] == this.list[secondhalf - 1]) {
+                secondhalf -= 1;
+                continue;
+            }
+            if (this.list[secondhalf] < this.list[secondhalf - 1]) {
+                swap(list, secondhalf, secondhalf - 1);
+                secondhalf -= 1;
+                continue;
+            }
+            if (this.list[secondhalf] > this.list[secondhalf - 1]) {
+                secondhalf -= 1;
+                continue;
+            }
+        }
+        while ((firsthalf % list.length) != 0) {
+            if (this.list[firsthalf] == this.list[firsthalf + 1]) {
+                firsthalf += 1;
+                continue;
+            }
+            if (this.list[firsthalf] <= this.list[firsthalf + 1]) {
+                firsthalf += 1;
+                continue;
+            }
+            if (this.list[firsthalf] > this.list[firsthalf + 1]) {
+                swap(list, firsthalf, firsthalf+1);
+                firsthalf += 1;
+                continue;
+            }
+            firsthalf += 1;
+        }
+    }
+
     public void swap(int[] l,int i, int k){
         int temp = l[i];
         l[i] = l[k];
