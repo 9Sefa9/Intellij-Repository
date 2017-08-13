6fb94b83c293b2ceb956387f7c1b39409342a74b
Sefa Computer <sefagoevercin@hotmail.de>
2017-08-13 19:25:19 +0200

    algorithm done

--- 4.Semester/src/Algorithms/Main.java
+++ 4.Semester/src/Algorithms/Main.java
@@ -3,7 +3,7 @@ package Algorithms;
 public class Main {
     public  static  void  main(String[] args) throws InterruptedException {
         int[] test ={23, 13, 2, 22, 10, 6, 8, 26, 7,4,30,1,21,19,27,17,5,12,18,15,16,20,3,25,24,9,29,14,28,11};
-        int[] test2 = {1,7,3,2,4,9,10,8,5,6};
+        int[] test2 = {20,18,50,7, 3, 4, 2, 9, 10, 8, 6, 5, 0, 8, 2, 11, 6, 12, 15, 14, 100, 20, 28, 29, 21, 60, 30, 50, 0, 7, 78,266};
         Splitsort s = new Splitsort(test);
         for(Integer i: test){
             System.out.print(i+", ");
--- 4.Semester/src/Algorithms/Splitsort.java
+++ 4.Semester/src/Algorithms/Splitsort.java
@@ -1,114 +1,55 @@
 package Algorithms;
 
-import java.util.ArrayList;
-
 /**
  * Splitsort Algorithm
  */
-public class Splitsort {
+public class Splitsort<T> {
     private int[] list;
     private int l,firsthalf,secondhalf;
 
     public Splitsort(int[] list){
-
         this.list = list;
-
-
     }
     /**
      * Splits the list in pieces
      */
     public void split() {
-        firsthalf =  list.length/2;
-        secondhalf=  list.length/2;
+        firsthalf = list.length / 2;
+        secondhalf = list.length / 2;
 
-        while (((secondhalf - 1) >= 0)) {
+        while (secondhalf - 1 >= 0 && secondhalf < list.length) {
 
-            if (this.list[secondhalf] == this.list[secondhalf - 1]) {
-                secondhalf -= 1;
-                continue;
-            }
-            if (this.list[secondhalf] < this.list[secondhalf - 1]) {
+            if ((this.list[secondhalf] < this.list[secondhalf - 1]) && secondhalf > 0) {
                 swap(list, secondhalf, secondhalf - 1);
+
+                if (secondhalf < list.length / 2)
+                    secondhalf += 1;
+
+                continue;
+            } else if ((this.list[secondhalf] >= this.list[secondhalf - 1]) && secondhalf > 0) {
                 secondhalf -= 1;
-                continue;
-            }
-            if (this.list[secondhalf] > this.list[secondhalf - 1]) {
-                secondhalf -= 1;
-                continue;
-            }
-        }
-            while ((firsthalf + 1) < list.length) {
-
-                if (this.list[firsthalf] == this.list[firsthalf + 1]) {
-                    firsthalf += 1;
-                    continue;
-                }
-                if (this.list[firsthalf] < this.list[firsthalf + 1]) {
-                    firsthalf += 1;
-                    continue;
-                }
-                if (this.list[firsthalf] > this.list[firsthalf + 1]) {
-                    swap(list, firsthalf, firsthalf+1);
-                    firsthalf += 1;
-                    continue;
-                }
-                firsthalf += 1;
-            }
-
-            /*
-                        1, 4, 2, 3,   7   , 6, 8, 9, 10,5
-                                      ij
-
-            */
-
-    }
-    public void alternateSplitSort(){
-        firsthalf  =  list.length/2;
-        secondhalf =  list.length/2;
-
-        while (secondhalf<=list.length/2 && secondhalf>0) {
-
-            if ((this.list[secondhalf] == this.list[secondhalf - 1]) && secondhalf>0) {
-                secondhalf -=1;
-                continue;
-            }
-            else if ((this.list[secondhalf] < this.list[secondhalf - 1]) && secondhalf>0) {
-                swap(list, secondhalf, secondhalf - 1);
-                secondhalf += 1;
-
-                continue;
-            }
-            else if ((this.list[secondhalf] > this.list[secondhalf - 1])&& secondhalf>0){
-                secondhalf -=1;
                 continue;
             }
             break;
         }
-        while (true) {
+        while (firsthalf < list.length && firsthalf >= 0) {
             //abbruchbedingung
-            if(firsthalf+1 == list.length){
+            if (firsthalf + 1 == list.length) {
                 break;
-            }
-            else if ((this.list[firsthalf] == this.list[firsthalf + 1]) && firsthalf <list.length-1) {
-                firsthalf += 1;
-                continue;
-            }
-            else if ((this.list[firsthalf] > this.list[firsthalf + 1]) && firsthalf <list.length-1) {
-                swap(list, firsthalf, firsthalf+1);
-                firsthalf -= 1;
+            } else if ((this.list[firsthalf] > this.list[firsthalf + 1]) && firsthalf < list.length - 1) {
+                swap(list, firsthalf, firsthalf + 1);
+
+                if (firsthalf > 0)
+                    firsthalf -= 1;
 
                 continue;
-            }
-            else if ((this.list[firsthalf] < this.list[firsthalf + 1]) && firsthalf <list.length-1) {
+            } else if ((this.list[firsthalf] <= this.list[firsthalf + 1]) && firsthalf < list.length - 1) {
                 firsthalf += 1;
                 continue;
             }
             break;
         }
-
     }
-
     public void swap(int[] l,int i, int k){
         int temp = l[i];
         l[i] = l[k];
