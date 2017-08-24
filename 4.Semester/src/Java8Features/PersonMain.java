465a12c8ff4c1d0b97b8406bb0be95ab72551e8a
Sefa Computer <sefagoevercin@hotmail.de>
2017-08-24 12:17:22 +0200

    nothing changed... learned a bit anonymous classes

---
+++ 4.Semester/src/Java8Features/PersonMain.java
@@ -0,0 +1,36 @@
+package Java8Features;
+
+//Kapitel: Anonyme Klassen
+public class PersonMain{
+    String name = "i";
+    public static void main(String[] args){
+        PersonMain s = new PersonMain();
+      s.test();
+    }
+    public void test(){
+        Person o = new Person(){
+
+            @Override
+            public void setName(String str){
+                name = str;
+            }
+
+            @Override
+            public String getName(){
+                return name;
+            }
+        };
+    }
+}
+
+class Person {
+    private String name;
+
+    public void setName(String str) {
+        this.name = str;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
