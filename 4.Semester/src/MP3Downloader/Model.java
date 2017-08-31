d3d6e316ef33eddd6324d9983175773a818dd8c4
Sefa Laptop <sefagoevercin@hotmail.de>
2017-08-31 22:22:32 +0200

    working on saving to a dir

--- 4.Semester/src/MP3Downloader/Model.java
+++ 4.Semester/src/MP3Downloader/Model.java
@@ -2,9 +2,25 @@ package MP3Downloader;
 
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.input.*;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Model {
+
+    private DirectoryChooser dirChooser;
+    private FileChooser fileChooser;
+    private File file,fileSave;
+    private Path path;
+    private String choosenPath;
+
     //attached to paste
     public void ctrlv(TextField urlfield){
         urlfield.setText(null);
@@ -20,6 +36,29 @@ public class Model {
           downloadList.getItems().add(url);
     }
     public void savePath(){
+            try{
+                fileChooser = new FileChooser();
+
+                //zeigt ein bevorzugtes format an , nämlich *.pl
+             //   FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("*.pl", ".pl");
+             //   fileChooser.getExtensionFilters().add(extFilter);
+
+                //zeigt den "save" Fenster
+                fileSave =fileChooser.showSaveDialog(new Stage());
+
+                if(fileSave != null)
+                    fileChooser.getInitialDirectory().getPath();
+             //   fileChooser.setTitle("Saving incoming MP3's in" +fileSave.getPath());
+
+                //solange fenster offen
+                if(fileSave!=null)
+                    //speichere die Dateien mit dem extension "*.mp3" in path
+                    this.choosenPath = fileSave.getPath();
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
 
     }
+
 }
