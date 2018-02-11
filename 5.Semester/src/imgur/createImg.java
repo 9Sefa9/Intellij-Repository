b93439fab41d84cb36c15f7f8cbcb374ab6bc157
Sefa Computer <sefagoevercin@hotmail.de>
2018-02-11 13:24:16 +0100

    -

--- 5.Semester/src/imgur/createImg.java
+++ 5.Semester/src/imgur/createImg.java
@@ -1,13 +1,17 @@
 package imgur;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.Buffer;
+import java.util.Scanner;
 import javax.imageio.*;
 
 public class createImg {
     public static void main(String[] args){
-        new createImg("G:/Users/Progamer/Desktop/test",500,500);
+      createImg t =  new createImg("G:/Users/Progamer/Desktop/hi.txt",1920 ,1080);
+        t.encrypt();
+        t.decrypt();
+        System.out.println("done");
     }
     private String path;
     private int width,height;
@@ -17,22 +21,62 @@ public class createImg {
         this.height = height;
 
     }
-    public void createImage(){
+
+    public void encrypt(){
+        DataInputStream dis = null;
         BufferedImage bi = null;
-        File f= null;
+        ByteArrayOutputStream byteArr = null;
+        FileOutputStream f= null;
         try{
             bi = new BufferedImage(width,height, BufferedImage.TYPE_INT_ARGB);
-            f = new File(this.path);
+            dis = new DataInputStream(new FileInputStream(this.path));
+            byteArr = new ByteArrayOutputStream();
 
-            int p = (23<<24) | (24<<16) | (25<<8) | 26; //pixel
+            int temp;
+            System.out.println((int)new FileInputStream(this.path).getChannel().size());
+            byte []buf2 =new byte[(int)new FileInputStream(this.path).getChannel().size()];
+            while((temp=dis.read(buf2,0,buf2.length))!= -1)
+                    byteArr.write(buf2,0,temp);
+
+            byteArr.flush();
+            byte[] ff = byteArr.toByteArray();
+
             for(int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    bi.setRGB(x, y, p);
+                for (int x = 0; x < width; x++){
+                    if(x<ff.length)
+                    bi.setRGB(x,y,ff[x]);
                 }
             }
             //write image:
-            ImageIO.write(bi,".png",f);
+            f = new FileOutputStream("G:/Users/Progamer/Desktop/hiENCRYPT.png");
+            ImageIO.write(bi,"png",f);
         }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    public void decrypt(){
+        DataInputStream dis= null;
+        BufferedImage bi = null;
+        try {
+            Scanner s = new Scanner(System.in);
+            s.nextLine();
+            dis = new DataInputStream(new FileInputStream("G:/Users/Progamer/Desktop/hiENCRYPT.png"));
+            bi = ImageIO.read(dis);
+
+
+            byte[] bufNew = new byte[(int)new FileInputStream("G:/Users/Progamer/Desktop/hiENCRYPT.png").getChannel().size()];
+            for(int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+
+                    bufNew[x]= (byte)bi.getRGB(x,y);
+                }
+            }
+
+
+           FileOutputStream fos = new FileOutputStream("G:/Users/Progamer/Desktop/hiDECRYPTED.txt");
+            fos.write(bufNew);
+            fos.flush();
+        }catch (IOException e){
             e.printStackTrace();
         }
     }
