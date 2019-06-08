0c109d1e3b036bb839bfcf541e444d40887347d9
9schurke9 <sefagoevercin@hotmail.de>
2019-06-08 02:02:16 +0200

    -

--- Project/src/ADBConnector/ADBConnect.java
+++ Project/src/ADBConnector/ADBConnect.java
@@ -3,15 +3,14 @@ package ADBConnector;
 import java.io.*;
 
 public class ADBConnect {
-        private static Process proc;
-        private static ProcessBuilder p;
+
     public static void main(String[] args) throws Exception {
-                String[] commands = {"cmd.exe", "/c","adb devices"};
-               p = new ProcessBuilder();
+                String[] commands = {"cmd.exe", "/c","adb devices && adb tcpip 5555 && adb connect 192.168.178.122 && adb devices"};
+               ProcessBuilder p = new ProcessBuilder();
                 p.command(commands);
 
                 p.redirectErrorStream(true);
-                proc = p.start();
+                Process proc  = p.start();
                    // ,"adb tcpip 5555","adb connect 192.168.178.122"
                 new Thread(()->{
                     try {
@@ -24,12 +23,30 @@ public class ADBConnect {
                         e.printStackTrace();
                     }
                 }).start();
-               if(!proc.isAlive()) {
-                   p.command("adb tcpip 5555");
-                   p.start();
-
-               }
+                proc.waitFor();
+                System.out.println("remove USB");
             }
 
+   /* public static void pull(String androidPath){
+        String[] commands = {"cmd.exe", "/c","adb pull"+androidPath};
+        ProcessBuilder processBuilder = new ProcessBuilder(commands);
+        processBuilder.redirectErrorStream(true);
+        try {
+            Process proc = processBuilder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        new Thread(()->{
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }).start();
 
+    }*/
 }
