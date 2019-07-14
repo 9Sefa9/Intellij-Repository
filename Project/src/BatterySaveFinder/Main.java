cb650510f4cc85694ae3acc1909596c823e78863
9schurke9 <diabolo31213>
2019-07-14 17:41:19 +0200

    -

---
+++ Project/src/BatterySaveFinder/Main.java
@@ -0,0 +1,11 @@
+package BatterySaveFinder;
+
+public class Main {
+    public static void main(String[] args){
+        Kernel32.SYSTEM_POWER_STATUS batteryStatus = new Kernel32.SYSTEM_POWER_STATUS();
+        Kernel32.INSTANCE.GetSystemPowerStatus(batteryStatus);
+
+        System.out.println(batteryStatus); // Shows result of toString() method.
+    }
+
+}
